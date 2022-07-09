1cb44a2817264b2a3660b78b48c0c55d986cf14e
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-09 06:00:38 +0000

    [Add]Editページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/show'
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -19,6 +19,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'books/new'
   get 'books/index'
   post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
 
   get '/' => 'homes#top'
 
