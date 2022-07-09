6787e76929cc052e6e4ea0974c9a98793ee55f4c
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-09 10:13:24 +0000

    [Add]バリ　エラーメッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if  book.save
-    redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    @book = Book.new(book_params)
+    if  @book.save
+    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
     end
   end
 
