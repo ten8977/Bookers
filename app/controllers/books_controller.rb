1041b0ba8d7fd722645b9ef95e4605400a38a031
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-09 09:05:17 +0000

    [Add]新規投稿時メッセ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,9 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if  book.save
+    redirect_to book_path(book.id), notice: 'Book was successfully created.'
+    end
   end
 
   def show
