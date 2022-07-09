719a68687db485ab5b747fdffd6be3af06be361f
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-09 07:39:20 +0000

    [Add]削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   get 'books/new'
-  get 'books/index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   get '/' => 'homes#top'
 
